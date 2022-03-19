d17758b24cd66ae4368c953b7fb0bb40685f914c
Slyz <contact.thegamingclub@gmail.com>
2022-03-19 10:12:26 +0100

    commit

--- src/components/TitleBar/TitleBar.jsx
+++ src/components/TitleBar/TitleBar.jsx
@@ -3,7 +3,6 @@ import './TitleBar.scss';
 import { faMinus, faX } from '@fortawesome/free-solid-svg-icons';
 import { faSquare } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { BrowserWindow } from 'electron';
 
 export default function TitleBar() {
     /*const win = remote.getCurrentWindow();
