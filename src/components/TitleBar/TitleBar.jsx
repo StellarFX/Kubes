2e02e63c6886f9d053405a8d4fe849de66c7ebab
StellarFX <priveeofficiel@gmail.com>
2022-03-19 10:13:43 +0100

    Removed "git" bug

--- src/components/TitleBar/TitleBar.jsx
+++ src/components/TitleBar/TitleBar.jsx
@@ -3,8 +3,15 @@ import './TitleBar.scss';
 import { faMinus, faX } from '@fortawesome/free-solid-svg-icons';
 import { faSquare } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ipcRenderer } from 'electron';
 
 export default function TitleBar() {
+
+    function closeWindow() {
+
+        ipcRenderer.send("close-window");
+
+    }
 
     return (
         <div className="titleBar">
