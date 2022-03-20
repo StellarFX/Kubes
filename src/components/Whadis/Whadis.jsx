96b8ea54e8676b7f3a760013c25bbbe6e956ee47
Slyz <contact.thegamingclub@gmail.com>
2022-03-20 11:54:29 +0100

    Little modifications on Whadis

--- src/components/Whadis/Whadis.jsx
+++ src/components/Whadis/Whadis.jsx
@@ -2,22 +2,20 @@ import { faQuestion } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Whadis.scss";
+import Logo from "../../public/Logo (1).svg"
 
 export default function Whadis() {
 
     return (
         <div className="whadis-container" id='wc'>
             <div className="title-container">
-                <FontAwesomeIcon icon={faQuestion} className="title-icon"/>
-                <p className="title">What is MsM</p>
+                <img src={Logo} className="title-icon"/>
+                <p className="title">Welcome to Kubes!</p>
             </div>
             <div className="main-text-container">
-                <p className="whadis">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                ut labore et dolore magna aliqua. <br/>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, Ut enim ad minim veniam, Ut enim ad minim veniam,Ut enim ad minim veniam,Ut enim ad minim veniam,Ut enim ad minim veniam,Ut enim ad minim veniam,</p>
+                <p className="whadis">What is Kubes? <br/> <br/>Kubes is a manager for Minecraft local servers.<br/>This software is here in order to make the setup of your servers easier
+                and shorter, therefore to make you have a better experience on the game.<br/> <br/> Kubes has been developped by two students in high school, and is moreover their first
+                software. We hope this will satisfy you and wish you a good experience!</p>
             </div>
         </div>
     );
